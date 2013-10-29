b5715864a3727dbd0c7ac9fea940afab7ef0ce12
Nevindy <jdoremus788@gmail.com>
2013-10-28 18:45:29 -0700

    First commit

---
+++ New Unity Project/Assets/Managers/GUIManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+
+	public GUIText gameOverText, instructionsText, runnerText;
+
+	// Use this for initialization
+	void Start () {
+		GameEventManager.GameStart += GameStart;
+		GameEventManager.GameOver += GameOver;
+		gameOverText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			GameEventManager.TriggerGameStart();
+		}
+	}
+	
+	private void GameStart() {
+		gameOverText.enabled = false;
+		instructionsText.enabled = false;
+		runnerText.enabled = false;
+		enabled = false;
+	}
+	
+	private void GameOver() {
+		gameOverText.enabled = true;
+		instructionsText.enabled = true;
+		enabled = true;
+	}
+}
---
+++ New Unity Project/Assets/Runner/Runner.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner : MonoBehaviour {
+
+	public static float distanceTraveled;
+	
+	public float acceleration;
+	public Vector3 jumpVelocity;
+	public float gameOverY;
+	
+	private bool touchingPlatform;
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		GameEventManager.GameStart += GameStart;
+		GameEventManager.GameOver += GameOver;
+		startPosition = transform.localPosition;
+		renderer.enabled = false;
+		rigidbody.isKinematic = true;
+		enabled = false;
+	}
+	
+	private void GameStart() {
+		distanceTraveled = 0f;
+		transform.localPosition = startPosition;
+		renderer.enabled = true;
+		rigidbody.isKinematic = false;
+		enabled = true;
+	}
+	
+	private void GameOver() {
+		renderer.enabled = false;
+		rigidbody.isKinematic = true;
+		enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(touchingPlatform && Input.GetButtonDown("Jump")){
+			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+			touchingPlatform = false;
+		}
+		distanceTraveled = transform.localPosition.x;
+		
+		if(transform.localPosition.y < gameOverY) {
+			GameEventManager.TriggerGameOver();
+		}
+	}
+	
+	void FixedUpdate () {
+		if(touchingPlatform) {
+			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+		}
+	}
+	
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
+	
+	void OnCollisionExit () {
+		touchingPlatform = false;
+	}
+}
